073ff67be5bbbf1e231d34f9e184efe0398e3365
merupeshpradhan <merupeshpradhan@gmail.com>
2023-04-09 23:07:01 +0530

    no1

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleBtn(){
+    var btn = document.getElementById("btn");
+    var light = document.getElementById("light");
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+
+}
